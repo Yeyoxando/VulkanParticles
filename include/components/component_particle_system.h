5660027a1d9966bf5efebaf7f6b05cd7f864ea47
YeyoXando <yeyoxando@gmail.com>
2021-04-15 15:32:27 +0200

    Commented particle system component in doxygen style.

--- include/components/component_particle_system.h
+++ include/components/component_particle_system.h
@@ -56,25 +56,25 @@ public:
 
 
 	// -- These setters will be part of the modules, for now they are hard-coded --
-	/// @brief Spawn particles in a box area. If not called particles will spawn in the same position
+	/// @brief Spawn particles in a box area. If not called particles will spawn in the same position.
 	void setSpawnArea(float length, float width, float height);
-	/// @brief 
+	/// @brief Time between two particles spawn.
 	void setEmissionRate(float emission_rate);
-	/// @brief 
+	/// @brief Time that a particle will live. If set to 0 it won't die.
 	void setLifetime(float lifetime);
-	/// @brief 
+	/// @brief Set a constant velocity for all the particles.
 	void setConstantVelocity(glm::vec3 constant_velocity);
-	/// @brief 
+	/// @brief Set a random initial velocity between the two given vectors.
 	void setInitialVelocity(glm::vec3 min_velocity, glm::vec3 max_velocity);
 	/// @brief Interpolates initial velocity with final velocity over lifetime.
 	void setVelocityOverTime(glm::vec3 final_velocity);
-	/// @brief 
+	/// @brief If called it will spawn all the particles in the same frame.
 	void setBurst();
-	/// @brief 
+	/// @brief Set a constant color for all the particles.
 	void setParticleColor(glm::vec4 color);
-	/// @brief 
+	/// @brief Set a color to change the particle over their life time.
 	void setParticleColorOverTime(glm::vec4 final_color);
-	/// @brief 
+	/// @brief Set an alpha value to change the particles over their life time.
 	void setAlphaColorOverTime(float final_alpha);
 
 
@@ -105,20 +105,27 @@ protected:
 	glm::vec4 initial_color_;
 
 	glm::vec3 initial_velocity_;
+	/// @brief Min velocity for the random distribution.
 	glm::vec3 min_velocity_;
+	/// @brief Max velocity for the random distribution.
 	glm::vec3 max_velocity_;
 
+	/// @brief Indicates if the color should interpolate over time.
 	bool lerp_color_;
+	/// @brief Indicates if the alpha value should interpolate over time.
 	bool lerp_alpha_;
+	/// @brief Color to lerp.
 	glm::vec4 final_color_;
+	/// @brief Indicates if the speed should interpolate over time.
 	bool lerp_speed_;
+	/// @brief Speed to lerp.
 	glm::vec3 final_speed_;
 
 	/// @brief Time that passes between two particles spawning.
 	float emission_rate_;
 	/// @brief If true all particles will spawn at the same time ignoring the emission rate.
 	bool burst_;
-	///
+	/// @brief If set, all the particles will share the same velocity.
 	bool constant_velocity_;
 
 	/// @brief Internal ID of the mesh used in the particle system. For now is always a quad.
--- tests/main_particles_scene.cpp
+++ tests/main_particles_scene.cpp
@@ -116,6 +116,111 @@ int main() {
 	}
 
 
+	// Particle system creation and setting
+	Entity* particle_system_fire_torch_right = new Entity();
+	{
+		particle_system_fire_torch_right->initAsArchetype(Entity::kArchetype_ParticleSystem);
+
+		// Transform affect to all particles
+		ComponentTransform* transform = static_cast<ComponentTransform*>
+			(particle_system_fire_torch_right->getComponent(Component::kComponentKind_Transform));
+		transform->translate(glm::vec3(0.63f, 0.69f, 0.26f));
+		transform->scale(glm::vec3(0.2f, 0.2f, 0.2f));
+
+		// Get particle system component
+		ComponentParticleSystem* ps = static_cast<ComponentParticleSystem*>
+			(particle_system_fire_torch_right->getComponent(Component::kComponentKind_ParticleSystem));
+		// Initialize particle system with max particles
+		ps->init(100);
+		ps->setLifetime(2.0f);
+		ps->setInitialVelocity(glm::vec3(-0.06f, -0.06f, 0.02f), glm::vec3(0.06f, 0.06f, 0.06f));
+		ps->setAlphaColorOverTime(0.0f);
+
+		ps->loadTexture("../../../resources/textures/fire.png");
+
+		scene->addEntity(particle_system_fire_torch_right, (int)ParticleEditor::MaterialParent::kMaterialParent_Particles);
+	}
+
+	// Particle system creation and setting
+	Entity* particle_system_smoke_torch_right = new Entity();
+	{
+		particle_system_smoke_torch_right->initAsArchetype(Entity::kArchetype_ParticleSystem);
+
+		// Transform affect to all particles
+		ComponentTransform* transform = static_cast<ComponentTransform*>
+			(particle_system_smoke_torch_right->getComponent(Component::kComponentKind_Transform));
+		transform->translate(glm::vec3(0.63f, 0.71f, 0.26f));
+		transform->scale(glm::vec3(0.4f, 0.4f, 0.4f));
+
+		// Get particle system component
+		ComponentParticleSystem* ps = static_cast<ComponentParticleSystem*>
+			(particle_system_smoke_torch_right->getComponent(Component::kComponentKind_ParticleSystem));
+		// Initialize particle system with max particles
+		ps->init(100);
+		ps->setLifetime(6.0f);
+		ps->setParticleColor(glm::vec4(1.0f, 1.0f, 1.0f, 0.17f));
+		ps->setInitialVelocity(glm::vec3(-0.06f, -0.06f, 0.08f), glm::vec3(0.06f, 0.06f, 0.12f));
+		ps->setAlphaColorOverTime(0.0f);
+
+		ps->loadTexture("../../../resources/textures/smoke.png");
+
+		scene->addEntity(particle_system_smoke_torch_right, (int)ParticleEditor::MaterialParent::kMaterialParent_Particles);
+	}
+
+
+	// Particle system creation and setting
+	Entity* particle_system_fire_torch_left = new Entity();
+	{
+		particle_system_fire_torch_left->initAsArchetype(Entity::kArchetype_ParticleSystem);
+
+		// Transform affect to all particles
+		ComponentTransform* transform = static_cast<ComponentTransform*>
+			(particle_system_fire_torch_left->getComponent(Component::kComponentKind_Transform));
+		transform->translate(glm::vec3(-0.56f, 0.67f, 0.25f));
+		transform->scale(glm::vec3(0.2f, 0.2f, 0.2f));
+
+		// Get particle system component
+		ComponentParticleSystem* ps = static_cast<ComponentParticleSystem*>
+			(particle_system_fire_torch_left->getComponent(Component::kComponentKind_ParticleSystem));
+		// Initialize particle system with max particles
+		ps->init(100);
+		ps->setLifetime(2.0f);
+		ps->setInitialVelocity(glm::vec3(-0.06f, -0.06f, 0.02f), glm::vec3(0.06f, 0.06f, 0.06f));
+		ps->setAlphaColorOverTime(0.0f);
+
+		ps->loadTexture("../../../resources/textures/fire.png");
+
+		scene->addEntity(particle_system_fire_torch_left, (int)ParticleEditor::MaterialParent::kMaterialParent_Particles);
+	}
+
+	// Particle system creation and setting
+	Entity* particle_system_smoke_torch_left = new Entity();
+	{
+		particle_system_smoke_torch_left->initAsArchetype(Entity::kArchetype_ParticleSystem);
+
+		// Transform affect to all particles
+		ComponentTransform* transform = static_cast<ComponentTransform*>
+			(particle_system_smoke_torch_left->getComponent(Component::kComponentKind_Transform));
+		transform->translate(glm::vec3(-0.56f, 0.69f, 0.25f));
+		transform->scale(glm::vec3(0.4f, 0.4f, 0.4f));
+
+		// Get particle system component
+		ComponentParticleSystem* ps = static_cast<ComponentParticleSystem*>
+			(particle_system_smoke_torch_left->getComponent(Component::kComponentKind_ParticleSystem));
+		// Initialize particle system with max particles
+		ps->init(100);
+		ps->setLifetime(6.0f);
+		//ps->setEmissionRate(0.3f);
+		ps->setParticleColor(glm::vec4(1.0f, 1.0f, 1.0f, 0.17f));
+		ps->setInitialVelocity(glm::vec3(-0.06f, -0.06f, 0.08f), glm::vec3(0.06f, 0.06f, 0.12f));
+		ps->setAlphaColorOverTime(0.0f);
+
+		ps->loadTexture("../../../resources/textures/smoke.png");
+
+		scene->addEntity(particle_system_smoke_torch_left, (int)ParticleEditor::MaterialParent::kMaterialParent_Particles);
+	}
+
+
 	// Set up scene to the app!!
 	ParticleEditor::instance().loadScene(scene);
 
